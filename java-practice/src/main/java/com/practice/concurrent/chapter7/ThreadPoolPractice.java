7f7258159e2cdc11dfdee23254c55ce4fac5fec9
Nemo <ljying2013@gmail.com>
2021-01-15 18:52:31 +0800

    添加线程池拒绝策略自定义实现

--- java-practice/src/main/java/com/practice/concurrent/chapter7/ThreadPoolPractice.java
+++ java-practice/src/main/java/com/practice/concurrent/chapter7/ThreadPoolPractice.java
@@ -1,7 +1,11 @@
 package com.practice.concurrent.chapter7;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -34,7 +38,7 @@ public class ThreadPoolPractice {
     /**
      * 阻塞队列：设置要求：有界队列，否则会撑爆内存
      */
-    private final BlockingQueue<Runnable> mBlockQueue = new LinkedBlockingQueue<>(100);
+    private final BlockingQueue<Runnable> mBlockQueue = new LinkedBlockingQueue<>(1);
 
     private final int mProcessorSize = Runtime.getRuntime().availableProcessors();
 
@@ -42,59 +46,111 @@ public class ThreadPoolPractice {
 
     private ThreadPoolExecutor mThreadPool;
 
+    //自定义拒绝策略，交给另外一个线程池去执行
+    private BlockingQueue<Runnable> mBackupTaskBlockQueue;
+    private ThreadPoolExecutor mBackupPool;
+    private RejectedExecutionHandler mCustomHandler = new RejectedExecutionHandler() {
+        @Override
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+            if (mBackupPool == null) {
+                mBackupTaskBlockQueue = new LinkedBlockingQueue<>(100);
+                mBackupPool = new ThreadPoolExecutor(mCorePoolSize, mProcessorSize, 20, TimeUnit.SECONDS, mBackupTaskBlockQueue);
+            }
+
+            mBackupPool.execute(() -> {
+                System.out.println("mBackupPool execute==");
+                runnable.run();
+            });
+        }
+    };
+
     public void initPool(int taskType) {
         if (taskType == TASK_TYPE_COMPUTE) {
             mThreadPool = new ThreadPoolExecutor(
-                    mCorePoolSize, mProcessorSize + 1, 20, TimeUnit.SECONDS, mBlockQueue);
+                    //拒绝策略，无法处理提交任务时由任务提交者执行
+                    mCorePoolSize, mProcessorSize + 1, 20, TimeUnit.SECONDS, mBlockQueue, new ThreadPoolExecutor.CallerRunsPolicy());
         } else if (taskType == TASK_TYPE_IO) {
             mThreadPool = new ThreadPoolExecutor(
-                    mCorePoolSize, mProcessorSize * 2, 20, TimeUnit.SECONDS, mBlockQueue);
+                    //拒绝策略：无法处理提交任务时由丢弃任务
+                    mCorePoolSize, mProcessorSize * 2, 20, TimeUnit.SECONDS, mBlockQueue, new ThreadPoolExecutor.DiscardPolicy());
+        } else if (taskType == TASK_MIX) {
+            mThreadPool = new ThreadPoolExecutor(
+                    //拒绝策略：无法处理提交任务时由丢弃最旧任务
+                    mCorePoolSize, mProcessorSize, 20, TimeUnit.SECONDS, mBlockQueue, new ThreadPoolExecutor.DiscardOldestPolicy());
         } else {
             mThreadPool = new ThreadPoolExecutor(
-                    mCorePoolSize, mProcessorSize, 20, TimeUnit.SECONDS, mBlockQueue);
+                    //拒绝策略：无法处理提交任务时抛出RejectedExecutionException异常
+                    mCorePoolSize, mProcessorSize, 20, TimeUnit.SECONDS, mBlockQueue, new ThreadPoolExecutor.AbortPolicy());
         }
+    }
+
+    public void switchToCustomRejectHandler() {
+        mThreadPool = new ThreadPoolExecutor(
+                //拒绝策略：自定义，当前执行不了任务交给备用线程池去执行
+                1, 1, 1, TimeUnit.SECONDS, mBlockQueue, mCustomHandler);
+        mThreadPool.setRejectedExecutionHandler(mCustomHandler);
     }
 
     public static void main(String[] args) {
 
-        ThreadPoolPractice cpuPool = new ThreadPoolPractice();
-        cpuPool.initPool(TASK_TYPE_COMPUTE);
+        ThreadPoolPractice testPool = new ThreadPoolPractice();
+        testPool.initPool(TASK_TYPE_COMPUTE);
 
         for (int i = 0; i < 10; i++) {
-            int finalI = i;
-            cpuPool.mThreadPool.execute(() -> {
-                System.out.println("add an task to ====" + finalI);
-            });
+            testPool.mThreadPool.execute(new RunTask("taskName" + i));
         }
+        testPool.mThreadPool.shutdown();
 
+        System.out.println("test custom reject policy==");
+        testCustomRejectPolicy();
+    }
 
-        ThreadPoolPractice ioPool = new ThreadPoolPractice();
-        ioPool.initPool(TASK_TYPE_IO);
+    static void testCustomRejectPolicy() {
 
-        ioPool.mThreadPool.execute(() -> {
+        ThreadPoolPractice testPool = new ThreadPoolPractice();
+        testPool.switchToCustomRejectHandler();
+        for (int i = 0; i < 10000; i++) {
+            Future<String> task = testPool.mThreadPool.submit(new CallableTask());
+            if (i == 0) {
+                for (int j = 0; j < 10; j++) {
+                    testPool.mThreadPool.execute(new RunTask("taskName" + j));
+                }
+            }
+
             try {
-                System.out.println("add io task=====");
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                System.out.println(task.get());
+            } catch (InterruptedException | ExecutionException e) {
+                testPool.mThreadPool.shutdown();
                 e.printStackTrace();
             }
-        });
+        }
+    }
 
+    static class RunTask implements Runnable {
 
-        ThreadPoolPractice mixPool = new ThreadPoolPractice();
-        mixPool.initPool(TASK_MIX);
+        private String mTaskName;
 
-        for (int i = 0; i < 20; i++) {
-            mixPool.mThreadPool.execute(() -> {
-                try {
-                    System.out.println("add mix task=====");
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
+        public RunTask(String mTaskName) {
+            this.mTaskName = mTaskName;
         }
 
+        @Override
+        public void run() {
+            System.out.println("execute run task::" + mTaskName);
+        }
     }
+
+    static class CallableTask implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            long beginTime = System.currentTimeMillis();
+            while (System.currentTimeMillis() - beginTime < TimeUnit.SECONDS.toMillis(5)) {
+                //DO haoshi operate
+            }
+            return "call task result::" + Thread.currentThread().getName();
+        }
+    }
+
 
 }
