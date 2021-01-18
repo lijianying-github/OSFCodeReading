194a9ebcc9744700e8f7dbd4c7c8494cb9b1f977
Nemo <ljying2013@gmail.com>
2021-01-18 15:55:00 +0800

    设计模式单例多线程创建模拟

---
+++ java-practice/src/main/java/com/practice/design_patterns/creational_patterns/singleton/KotlinSingleton.kt
@@ -0,0 +1,10 @@
+package com.practice.design_patterns.creational_patterns.singleton
+
+/**
+ * Description:kotlin版单例实现原理饿汉式静态方法创建
+ *
+ * @author Li Jianying
+ * @version 1.0
+ * @since 2021/1/18
+ */
+object KotlinSingleton
--- java-practice/src/main/java/com/practice/design_patterns/creational_patterns/singleton/SingletonTest.java
+++ java-practice/src/main/java/com/practice/design_patterns/creational_patterns/singleton/SingletonTest.java
@@ -1,5 +1,7 @@
 package com.practice.design_patterns.creational_patterns.singleton;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Description:
  *
@@ -10,19 +12,69 @@ package com.practice.design_patterns.creational_patterns.singleton;
 public class SingletonTest {
 
     public static void main(String[] args) {
+        testMultiThreadCreate();
+        testMultiThreadReflectCreate();
+    }
+
+    /**
+     * 多线程创建对象模拟
+     */
+    private static void testMultiThreadCreate() {
         for (int i = 0; i < 10; i++) {
 
             new Thread(() -> {
-
-                HungrySingleton hungrySingleton = HungrySingleton.getInstance();
-                System.out.println("hungrySingleton==" + hungrySingleton.hashCode()+"=="+ Thread.currentThread().getName());
-
-                DoubleCheckLazySingleton doubleCheckLazySingleton = DoubleCheckLazySingleton.getInstance();
-                System.out.println("doubleCheckLazySingleton==" + doubleCheckLazySingleton.hashCode()+"=="+ Thread.currentThread().getName());
-
-                InnerClazzLazySingleton innerClazzLazySingleton = InnerClazzLazySingleton.getInstance();
-                System.out.println("innerClazzLazySingleton==" + innerClazzLazySingleton.hashCode()+"=="+ Thread.currentThread().getName());
+                testHungrySingleton();
+                testKotlinSingleton();
+                testDoubleCheckLazySingleton();
+                testInnerClazzLazySingleton();
             }).start();
+        }
+    }
+
+    /**
+     * 多线程反射常见的对象模拟
+     */
+    private static void testMultiThreadReflectCreate() {
+        for (int i = 0; i < 10; i++) {
+
+            new Thread(() -> {
+                testSingletonReflectInvoke(HungrySingleton.class);
+                testSingletonReflectInvoke(InnerClazzLazySingleton.class);
+                testSingletonReflectInvoke(DoubleCheckLazySingleton.class);
+                testSingletonReflectInvoke(KotlinSingleton.class);
+            }).start();
+        }
+    }
+
+    private static void testHungrySingleton() {
+        HungrySingleton hungrySingleton = HungrySingleton.getInstance();
+        System.out.println("hungrySingleton==" + hungrySingleton.hashCode() + "==" + Thread.currentThread().getName());
+    }
+
+    private static void testInnerClazzLazySingleton() {
+        InnerClazzLazySingleton innerClazzLazySingleton = InnerClazzLazySingleton.getInstance();
+        System.out.println("innerClazzLazySingleton==" + innerClazzLazySingleton.hashCode() + "==" + Thread.currentThread().getName());
+    }
+
+    private static void testDoubleCheckLazySingleton() {
+        DoubleCheckLazySingleton doubleCheckLazySingleton = DoubleCheckLazySingleton.getInstance();
+        System.out.println("doubleCheckLazySingleton==" + doubleCheckLazySingleton.hashCode() + "==" + Thread.currentThread().getName());
+    }
+
+    //kotlin 版单例创建，反射可以创建多个对象
+    private static void testKotlinSingleton() {
+        KotlinSingleton kotlinSingleton = KotlinSingleton.INSTANCE;
+        System.out.println("kotlinSingleton==" + kotlinSingleton.hashCode() + "==" + Thread.currentThread().getName());
+    }
+
+    private static <T> void testSingletonReflectInvoke(Class<T> clazz) {
+        try {
+            Constructor<T> kotlinSingletonConstructor = clazz.getDeclaredConstructor();
+            kotlinSingletonConstructor.setAccessible(true);
+            T reflectInstance = kotlinSingletonConstructor.newInstance();
+            System.out.println("kotlinSingleton reflect==" + reflectInstance.hashCode() + "==" + Thread.currentThread().getName());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
